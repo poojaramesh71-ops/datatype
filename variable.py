0f478d60b09f59d4762dff3e9ab70e7e18c991e4
Pooja Ramesh <kumbarp71@gmail.com>
2026-01-08 14:17:22 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,16 @@
+name = "pooja"
+age = 10
+height = 1.20   # height in meters
+student = True
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+print(student)
+print(type(student))
